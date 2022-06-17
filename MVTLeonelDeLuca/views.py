dc6d5d9e8b649cdbc91935f8e913d23bdba53be3
leo-de-luca <leoneldeluca@hotmail.com>
2022-06-16 22:21:06 -0300

    primer commit

---
+++ MVTLeonelDeLuca/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+from django.template import loader
+
+def elNombreEs(self,nombre):
+    documentoDeTexto = f"Mi nombre es: <br><br> {nombre}"
+def elApellidoEs(self,apellido):
+
+def anioQueNacio(self,anio):
+
+def edadActual(self,edad):
+    
